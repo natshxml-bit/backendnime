1bcf64f81d02d9483c1e30b7f742ec4806322add
Sidz <sidzforyou752@gmail.com>
2026-08-31 00:42:53 +0700

    chore: remove S3 cache bust (now 10 cached)

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const adapter = require("./adapter");
 const db = require("./db/db");
-db.del("anime:mushoku-ni-tensei-s3-sub-indo").catch(()=>{});
 const fs = require("fs");
 const path = require("path");
 
